fe2883971070ac4c41c3c1d2851506e02c0e86bd
Amalia <e41230769@student.polije.ac.id>
2024-11-20 08:44:37 +0700

    update logo

--- data-donasi.php
+++ data-donasi.php
@@ -68,7 +68,7 @@ try {
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo_peduly.png" alt="">
-        <span class="d-none d-lg-block">PedulyJember</span>
+        <span class="d-none d-lg-block">Peduly</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
